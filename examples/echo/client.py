4779bb0f32e8c2c416a8af54baf8b2fd1c741523
Travis J Parker <travis.parker@gmail.com>
2010-07-23 15:25:59 -0700

    echo server/client example

---
+++ examples/echo/client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# vim: fileencoding=utf8:et:sta:ai:sw=4:ts=4:sts=4
+
+from notifier import access, publish
+
+
+ECHO = "echo"
+NOTIFIERS = [('localhost', 7000)]
+
+
+def main():
+    pub = publish.SimpleNotifyPublisher(NOTIFIERS)
+
+    while 1:
+        string = raw_input()
+
+        if not string.strip():
+            break
+
+        result = access._execute(
+                'echo',
+                pub,
+                ECHO,
+                hash(string),
+                args=(string,))
+
+        print result
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/echo/server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# vim: fileencoding=utf8:et:sta:ai:sw=4:ts=4:sts=4
+
+import logging
+import signal
+
+from gogreen import backdoor, coro
+from notifier import decorators, notifier, service
+
+
+ECHO = "echo"
+NOTIFIERS = [('localhost', 7000)]
+
+
+class EchoWorker(service.Worker):
+    @decorators.command
+    def echo(self, id, string):
+        return "echo: " + str(string)
+
+
+class EchoServer(service.Server):
+    worker_class = EchoWorker
+    subscription = ECHO
+
+
+def main():
+    ns = notifier.NotifyServer(args=(NOTIFIERS,), kwargs={'port': 7000})
+    ns.start()
+
+    server = EchoServer(
+            100,
+            notifier=ns,
+            bounds={'mask': 0, 'value': 0},
+            loglevel=logging.INFO)
+
+    def shutdown(signum, frame):
+        coro.spawn(lambda: server.drain() and ns.shutdown())
+    signal.signal(signal.SIGINT, shutdown)
+
+    coro.event_loop()
+
+
+if __name__ == '__main__':
+    main()
